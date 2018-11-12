f709f21ae7f1a6d4b04be87ceec03c4c5eae6341
IINemo <ArtemShelmanov@gmail.com>
2018-11-13 01:12:15 +0300

    Support only 4 tests for version 0.01

--- src/test_docker/config.py
+++ src/test_docker/config.py
@@ -10,9 +10,7 @@ DOCKER_MODULE_LIST = [
     dict(image_name='inemo/syntaxnet_rus',
          test_script='run_test_syntaxnet'),
     dict(image_name='inemo/isanlp_srl_framebank',
-         test_script='run_test_srl_framebank'),
-    dict(image_name='inemo/isanlp_parser_conll2008',
-         test_script='run_test_parser_conll2008')
+         test_script='run_test_srl_framebank')
 ]
 
 
