2149f87f41ca7a293020480e845b40934f3102a0
IINemo <ArtemShelmanov@gmail.com>
2018-11-13 04:17:23 +0300

    Add script for docker push

---
+++ src/tools/push_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker push inemo/isanlp_base 
+docker push inemo/isanlp 
+docker push inemo/isanlp_srl_framebank 
+docker push inemo/isanlp_parser_conll2008 
+docker push inemo/isanlp_udpipe
